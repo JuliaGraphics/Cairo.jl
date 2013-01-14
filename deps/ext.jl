781fadc108e292fe71c68f91cb70888f4a34e94b
Keno Fischer <kfischer@college.harvard.edu>
2013-01-13 23:36:22 -0500

    Fix ext.jl

--- deps/ext.jl
+++ deps/ext.jl
@@ -1,4 +1,4 @@
-ccall(:add_library_mapping,Int32,(Ptr{Uint8},Ptr{Uint8}),"libcairo",dlopen(joinpath(julia_pkgdir(),"Cairo","deps","usr","lib","libcairo"))
-ccall(:add_library_mapping,Int32,(Ptr{Uint8},Ptr{Uint8}),"libpango-1.0",dlopen(joinpath(julia_pkgdir(),"Cairo","deps","usr","lib","libpango-1.0"))
-ccall(:add_library_mapping,Int32,(Ptr{Uint8},Ptr{Uint8}),"libpangocairo-1.0",dlopen(joinpath(julia_pkgdir(),"Cairo","deps","usr","lib","libpangocairo-1.0"))
-ccall(:add_library_mapping,Int32,(Ptr{Uint8},Ptr{Uint8}),"libgobject-2.0",dlopen(joinpath(julia_pkgdir(),"Cairo","deps","usr","lib","libgobject-2.0"))
+ccall(:add_library_mapping,Int32,(Ptr{Uint8},Ptr{Uint8}),"libcairo",dlopen(joinpath(julia_pkgdir(),"Cairo","deps","usr","lib","libcairo")))
+ccall(:add_library_mapping,Int32,(Ptr{Uint8},Ptr{Uint8}),"libpango-1.0",dlopen(joinpath(julia_pkgdir(),"Cairo","deps","usr","lib","libpango-1.0")))
+ccall(:add_library_mapping,Int32,(Ptr{Uint8},Ptr{Uint8}),"libpangocairo-1.0",dlopen(joinpath(julia_pkgdir(),"Cairo","deps","usr","lib","libpangocairo-1.0")))
+ccall(:add_library_mapping,Int32,(Ptr{Uint8},Ptr{Uint8}),"libgobject-2.0",dlopen(joinpath(julia_pkgdir(),"Cairo","deps","usr","lib","libgobject-2.0")))
