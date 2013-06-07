ac196bdb42070d138c11b24aa657cdfa83c70d6e
Viral B. Shah <viral@mayin.org>
2013-06-07 16:44:57 +0530

    Revert "Refactor find_library code"
    This reverts commit abb2503c586ab1061a0a2eeeda34e227b67006c5.

--- deps/ext.jl
+++ deps/ext.jl
@@ -1,13 +1,20 @@
-let
-    printerror() = error("Failed to find required library "*libname*". Try re-running the package script using Pkg.runbuildscript(\"pkg\")")
-
-    if (!find_library("Cairo", "libcairo",OS_NAME == :Windows ? "libcairo-2" : "libcairo") ||
-        !find_library("Cairo", "libfontconfig",OS_NAME == :Windows ? "libfontconfig-1" : "libfontconfig") ||
-        !find_library("Cairo", "libpango-1.0",OS_NAME == :Windows ? "libpango-1.0-0" : "libpango-1.0") ||
-        !find_library("Cairo", "libpangocairo-1.0",OS_NAME == :Windows ? "libpangocairo-1.0-0" : "libpangocairo-1.0") ||
-        !find_library("Cairo", "libgobject-2.0",OS_NAME == :Windows ? "libgobject-2.0-0" : "libgobject-2.0")
-        )
-        printerror()
+let 
+    function find_library(libname,filename)
+        try 
+            dl = dlopen(joinpath(Pkg.dir(),"Cairo","deps","usr","lib",filename))
+            ccall(:add_library_mapping,Int32,(Ptr{Uint8},Ptr{Uint8}),libname,dl)
+        catch
+            try 
+                dl = dlopen(libname)
+                dlclose(dl)
+            catch
+                error("Failed to find required library "*libname*". Try re-running the package script using Pkg.runbuildscript(\"pkg\")")
+            end
+        end
     end
-
+    find_library("libcairo",OS_NAME == :Windows ? "libcairo-2" : "libcairo")
+    find_library("libfontconfig",OS_NAME == :Windows ? "libfontconfig-1" : "libfontconfig")
+    find_library("libpango-1.0",OS_NAME == :Windows ? "libpango-1.0-0" : "libpango-1.0")
+    find_library("libpangocairo-1.0",OS_NAME == :Windows ? "libpangocairo-1.0-0" : "libpangocairo-1.0")
+    find_library("libgobject-2.0",OS_NAME == :Windows ? "libgobject-2.0-0" : "libgobject-2.0")
 end
