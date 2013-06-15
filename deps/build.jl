072d6e995ace33485e5080f8260048b0643d911e
Viral B. Shah <viral@mayin.org>
2013-06-15 15:55:55 +0530

    Fix build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -84,10 +84,10 @@ end # build()
 
 builddeps = false
 
-find_library("Cairo", "libcairo", ["libcairo-2", "libcairo"]) || builddeps = true
-find_library("Cairo", "libfontconfig", ["libfontconfig-1", "libfontconfig"]) || builddeps = true 
-find_library("Cairo", "libpango",["libpango-1.0-0", "libpango-1.0"]) || builddeps = true
-find_library("Cairo", "libpangocairo", ["libpangocairo-1.0-0", "libpangocairo-1.0"]) || builddeps = true
-find_library("Cairo", "libgobject", ["libgobject-2.0-0", "libgobject-2.0"]) || builddeps = true
+find_library("Cairo", "libcairo", ["libcairo-2", "libcairo"]) || (builddeps = true)
+find_library("Cairo", "libfontconfig", ["libfontconfig-1", "libfontconfig"]) || (builddeps = true)
+find_library("Cairo", "libpango",["libpango-1.0-0", "libpango-1.0"]) || (builddeps = true)
+find_library("Cairo", "libpangocairo", ["libpangocairo-1.0-0", "libpangocairo-1.0"]) || (builddeps = true)
+find_library("Cairo", "libgobject", ["libgobject-2.0-0", "libgobject-2.0"]) || (builddeps = true)
 
 builddeps && build()
