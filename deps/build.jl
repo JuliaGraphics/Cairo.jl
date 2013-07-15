3c84b0eeca4640d151f43691994c3dabaaccd82d
Jameson Nash <vtjnash@gmail.com>
2013-07-15 18:09:18 -0400

    fix find_library usage to remove deprecation warnings

--- deps/build.jl
+++ deps/build.jl
@@ -82,11 +82,12 @@ function build()
 
 end # build()
 
-alllibs = find_library("Cairo", "libcairo", ["libcairo-2", "libcairo"]) && 
-          find_library("Cairo", "libfontconfig", ["libfontconfig-1", "libfontconfig"]) &&
-          find_library("Cairo", "libpango",["libpango-1.0-0", "libpango-1.0"]) &&
-          find_library("Cairo", "libpangocairo", ["libpangocairo-1.0-0", "libpangocairo-1.0"]) &&
-          find_library("Cairo", "libgobject", ["libgobject-2.0-0", "libgobject-2.0"])
+alllibs = try
+    evalfile("ext.jl")
+    true
+catch
+    false
+end
 
 if !alllibs; build(); end
 
