c9e075aa1c20c9678bad5f4a0a63d37f4cf3ca38
lobi <lobingera>
2014-01-03 14:15:48 +0100

    rm deps/deps.jl which is automagically created, path+text_extends working

--- deps/deps.jl
+++
@@ -1,8 +0,0 @@
-macro checked_lib(libname, path)
-        (dlopen_e(path) == C_NULL) && error("Unable to load \n\n$libname ($path)\n\nPlease re-run Pkg.build(package), and restart Julia.")
-        quote const $(esc(libname)) = $path end
-    end
-@checked_lib _jl_libgobject "libgobject-2.0"
-@checked_lib _jl_libpangocairo "libpangocairo-1.0"
-@checked_lib _jl_libcairo "libcairo"
-@checked_lib _jl_libpango "libpango-1.0"
