d744be542d423e2c6c72f849bda7a41c98d3ba8b
lobi <lobingera>
2014-01-04 20:49:14 +0100

    pick up sample files,

---
+++ samples/sample_curve_to.jl
@@ -0,0 +1,38 @@
+## header to provide surface and context
+using Cairo
+c = CairoRGBSurface(256,256);
+cr = CairoContext(c);
+
+save(cr);
+set_source_rgb(cr,0.8,0.8,0.8);    # light gray
+rectangle(cr,0.0,0.0,256.0,256.0); # background
+fill(cr);
+restore(cr);
+
+save(cr);
+
+## original example, following here
+
+x=25.6;  y=128.0;
+x1=102.4; y1=230.4;
+x2=153.6; y2=25.6;
+x3=230.4; y3=128.0;
+
+move_to (cr, x, y);
+curve_to (cr, x1, y1, x2, y2, x3, y3);
+
+set_line_width (cr, 10.0);
+stroke (cr);
+
+set_source_rgba (cr, 1, 0.2, 0.2, 0.6);
+set_line_width (cr, 6.0);
+move_to (cr,x,y);   line_to (cr,x1,y1);
+move_to (cr,x2,y2); line_to (cr,x3,y3);
+stroke (cr);
+
+## mark picture with current date
+restore(cr);
+move_to(cr,0.0,12.0);
+set_source_rgb (cr, 0,0,0);
+show_text(cr,strftime(time()));
+write_to_png(c,"sample_curve_to.png");
---
+++ samples/sample_dash.jl
@@ -0,0 +1,39 @@
+## header to provide surface and context
+using Cairo
+c = CairoRGBSurface(256,256);
+cr = CairoContext(c);
+
+save(cr);
+set_source_rgb(cr,0.8,0.8,0.8);    # light gray
+rectangle(cr,0.0,0.0,256.0,256.0); # background
+fill(cr);
+restore(cr);
+
+save(cr);
+## original example, following here
+
+dashes = [50.0,  # ink 
+          10.0,  # skip
+          10.0,  # ink 
+          10.0   # skip
+          ];
+#ndash = length(dashes); not implemented as ndash on set_dash
+offset = -50.0;
+
+set_dash (cr, dashes, offset); 
+set_line_width (cr, 10.0);
+
+move_to (cr, 128.0, 25.6);
+line_to (cr, 230.4, 230.4);
+rel_line_to (cr, -102.4, 0.0);
+curve_to (cr, 51.2, 230.4, 51.2, 128.0, 128.0, 128.0);
+
+stroke (cr);
+
+
+## mark picture with current date
+restore(cr);
+move_to(cr,0.0,12.0);
+set_source_rgb (cr, 0,0,0);
+show_text(cr,strftime(time()));
+write_to_png(c,"sample_dash.png");
