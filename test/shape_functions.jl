43e9be5010800ce166fb45f16c0215b01a72bfeb
lobi <lobingera>
2014-03-06 07:21:27 +0100

    some indentation, runtests.jl (only import, no function)

--- test/shape_functions.jl
+++ test/shape_functions.jl
@@ -1,14 +1,14 @@
 using Cairo
 
 # shape functions, 
-# n_dotsx: fill random dots with different methods, disc only (no ring)
+
 
 function randpos(n,w::Real,h::Real)
     srand(141413);
     px = rand(n)*w;
     py = rand(n)*h;
     return (px,py);
-  end
+end
 
 function clear_bg(c::CairoContext,w::Real,h::Real)
     save(c);
@@ -18,6 +18,7 @@ function clear_bg(c::CairoContext,w::Real,h::Real)
     restore(c);
 end
 
+# ddotsx: fill random dots with different methods, disc only (no ring)
 function ddots1(cr::CairoContext, rect_width::Real, rect_height::Real, radius::Real, n::Int)
     
     clear_bg(cr,rect_width,rect_height);
@@ -28,7 +29,7 @@ function ddots1(cr::CairoContext, rect_width::Real, rect_height::Real, radius::R
        move_to(cr,px[i],py[i]);
        rel_move_to(cr,radius,0);
        arc(cr, px[i], py[i], radius, 0, 2*pi);
-       end
+    end
     close_path(cr);
 
     set_source_rgb(cr, 0, 0, 1.0);
@@ -41,14 +42,13 @@ function ddots2(cr::CairoContext, rect_width::Real, rect_height::Real, radius::R
     px,py = randpos(n,rect_width,rect_height);
 
     for i=1:n
-    new_path(cr);    
-       move_to(cr,px[i],py[i]);
-       rel_move_to(cr,radius,0);
-       arc(cr, px[i], py[i], radius, 0, 2*pi);
-    close_path(cr);
-
-    set_source_rgb(cr, 0, 0, 1.0);
-    fill(cr);
+        new_path(cr);    
+        move_to(cr,px[i],py[i]);
+        rel_move_to(cr,radius,0);
+        arc(cr, px[i], py[i], radius, 0, 2*pi);
+        close_path(cr);
+        set_source_rgb(cr, 0, 0, 1.0);
+        fill(cr);
     end
 end
 
@@ -68,11 +68,11 @@ function ddots3(cr::CairoContext, rect_width::Real, rect_height::Real, radius::R
        move_to(cr,px[i],py[i]);
        rel_line_to(cr,0,0);
        stroke(cr);
-       end
+    end
        
     close_path (cr);
 
-    end
+end
 
 function ddots4(cr::CairoContext,
     rect_width::Real, rect_height::Real, radius::Real, n::Int)
@@ -96,15 +96,15 @@ function ddots4(cr::CairoContext,
     py = rand(n,1)*rect_height;
     
     for i=1:n
-       save(cr);
-       translate(cr,px[i]-cc,py[i]-cc);
-       set_source(cr,p);
-       paint(cr);
-       restore(cr);
-       end
+        save(cr);
+        translate(cr,px[i]-cc,py[i]-cc);
+        set_source(cr,p);
+        paint(cr);
+        restore(cr);
     end
+end
 
-
+# lines0, random x,y lines
 function lines0(cr::CairoContext, rect_width::Real, rect_height::Real, width::Real, n::Int)
     
     clear_bg(cr,rect_width,rect_height);
@@ -119,12 +119,11 @@ function lines0(cr::CairoContext, rect_width::Real, rect_height::Real, width::Re
     move_to(cr,px[1],py[1]);
     for i=2:n
        line_to(cr,px[i],py[i]);
-
     end
     stroke(cr);
-      
-    end
+end
 
+# lines1, x sorted, line a plot
 function lines1(cr::CairoContext, rect_width::Real, rect_height::Real, width::Real, n::Int)
     
     clear_bg(cr,rect_width,rect_height);
@@ -138,12 +137,12 @@ function lines1(cr::CairoContext, rect_width::Real, rect_height::Real, width::Re
     new_path(cr);
     move_to(cr,px[1],py[1]);
     for i=2:n
-       line_to(cr,px[i],py[i]);
+        line_to(cr,px[i],py[i]);
     end
-    stroke(cr);
-      
-    end
+    stroke(cr);      
+end
 
+# lines2, x sorted, independent lines per coord
 function lines2(cr::CairoContext, rect_width::Real, rect_height::Real, width::Real, n::Int)
     
     clear_bg(cr,rect_width,rect_height);
@@ -156,9 +155,9 @@ function lines2(cr::CairoContext, rect_width::Real, rect_height::Real, width::Re
     
     move_to(cr,px[1],py[1]);
     for i=1:n-1
-       new_path(cr);
-       move_to(cr,px[i],py[i]);
-       line_to(cr,px[i+1],py[i+1]);
-       stroke(cr);
+        new_path(cr);
+        move_to(cr,px[i],py[i]);
+        line_to(cr,px[i+1],py[i+1]);
+        stroke(cr);
     end      
 end
