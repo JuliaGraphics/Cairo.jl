f52a327418cc444bf7e1a35f8bfd40dd0bfd1d0b
lobi <lobingera>
2014-03-09 11:59:39 +0100

    rdots as additional shape, some clarification, speed_test collects the timing in test_data, flag for saving into .png

--- test/shape_functions.jl
+++ test/shape_functions.jl
@@ -62,7 +62,7 @@ function ddots3(cr::CairoContext, rect_width::Real, rect_height::Real, radius::R
 
     set_source_rgb(cr, 0, 0, 1.0);
     set_line_cap(cr,Cairo.CAIRO_LINE_CAP_ROUND)
-    set_line_width(cr,radius)
+    set_line_width(cr,radius*2.0)
     
     for i=1:n
        move_to(cr,px[i],py[i]);
@@ -82,19 +82,15 @@ function ddots4(cr::CairoContext,
 
     cc = radius + 1
 
-    rectangle(cr,0,0,2*cc,2*22);
+    rectangle(cr,0,0,2*cc,2*cc);
     clip(cr);
     push_group(cr);
-    arc(cr,0,0,radius,0,2*pi);
+    arc(cr,radius,radius,radius,0,2*pi);
     set_source_rgb(cr,0,0,1.0);
     fill(cr);
     p = pop_group(cr);
     reset_clip(cr);
-    
-    new_path(cr);
-    px = rand(n,1)*rect_width;
-    py = rand(n,1)*rect_height;
-    
+        
     for i=1:n
         save(cr);
         translate(cr,px[i]-cc,py[i]-cc);
@@ -103,6 +99,166 @@ function ddots4(cr::CairoContext,
         restore(cr);
     end
 end
+
+function ddots5(cr::CairoContext,
+    rect_width::Real, rect_height::Real, radius::Real, n::Int)
+    
+    clear_bg(cr,rect_width,rect_height);
+    px,py = randpos(n,rect_width,rect_height);
+
+    cc = radius + 1;
+
+    s1 = Cairo.CairoARGBSurface(cc*2,cc*2);
+    c1 = Cairo.CairoContext(s1);
+    rectangle(c1,0,0,2*cc,2*cc);
+    set_source_rgba(c1,0,0,0,0);
+    paint(c1);
+    new_path(c1);
+    arc(c1,radius,radius,radius,0,2*pi);
+    set_source_rgb(c1,0,0,1.0);
+    fill(c1);
+
+    p = Cairo.CairoPattern(s1);
+        
+    for i=1:n
+        save(cr);
+        translate(cr,px[i]-cc,py[i]-cc);
+        set_source(cr,p);
+        paint(cr);
+        restore(cr);
+    end
+end
+
+# rdotsx: fill random dots with different methods
+function rdots1(cr::CairoContext, rect_width::Real, rect_height::Real, radius::Real, n::Int)
+    
+    clear_bg(cr,rect_width,rect_height);
+    px,py = randpos(n,rect_width,rect_height);
+    
+    new_path(cr);
+    for i=1:n
+       move_to(cr,px[i],py[i]);
+       rel_move_to(cr,radius,0);
+       arc(cr, px[i], py[i], radius, 0, 2*pi);
+    end
+    close_path(cr);
+
+    set_source_rgb(cr, 0, 0, 1.0);
+    fill_preserve(cr);
+    set_line_width(cr,1.0);
+    set_source_rgb(cr, 0, 0, 0);
+    stroke(cr);
+end
+
+function rdots2(cr::CairoContext, rect_width::Real, rect_height::Real, radius::Real, n::Int)
+
+    clear_bg(cr,rect_width,rect_height);
+    px,py = randpos(n,rect_width,rect_height);
+
+    for i=1:n
+        new_path(cr);    
+        move_to(cr,px[i],py[i]);
+        rel_move_to(cr,radius,0);
+        arc(cr, px[i], py[i], radius, 0, 2*pi);
+        close_path(cr);
+        set_source_rgb(cr, 0, 0, 1.0);
+        fill_preserve(cr);
+        set_line_width(cr,1.0);
+        set_source_rgb(cr, 0, 0, 0);
+        stroke(cr);
+    end
+end
+
+
+function rdots3(cr::CairoContext, rect_width::Real, rect_height::Real, radius::Real, n::Int)
+    
+    clear_bg(cr,rect_width,rect_height);
+    px,py = randpos(n,rect_width,rect_height);
+    
+    new_path(cr);
+
+    set_line_cap(cr,Cairo.CAIRO_LINE_CAP_ROUND);
+
+    set_source_rgb(cr, 0, 0, 0);
+    set_line_width(cr,radius*2 + 2.0);
+    for i=1:n
+       move_to(cr,px[i],py[i]);
+       rel_line_to(cr,0,0);
+       stroke(cr);
+    end
+
+    set_source_rgb(cr, 0, 0, 1.0);
+    set_line_width(cr,radius*2);
+    for i=1:n
+       move_to(cr,px[i],py[i]);
+       rel_line_to(cr,0,0);
+       stroke(cr);
+    end
+
+       
+end
+
+function rdots4(cr::CairoContext,
+    rect_width::Real, rect_height::Real, radius::Real, n::Int)
+    
+    clear_bg(cr,rect_width,rect_height);
+    px,py = randpos(n,rect_width,rect_height);
+
+    cc = radius + 3.0;
+
+    rectangle(cr,0,0,2*cc,2*cc);
+    clip(cr);
+    push_group(cr);
+    arc(cr,radius,radius,radius,0,2*pi);
+    set_source_rgb(cr,0,0,1.0);
+    fill_preserve(cr);
+    set_source_rgb(cr,0,0,0);
+    set_line_width(cr,1.0);
+    stroke(cr);
+    p = pop_group(cr);
+    reset_clip(cr);
+        
+    for i=1:n
+        save(cr);
+        translate(cr,px[i]-cc,py[i]-cc);
+        set_source(cr,p);
+        paint(cr);
+        restore(cr);
+    end
+end
+
+function rdots5(cr::CairoContext,
+    rect_width::Real, rect_height::Real, radius::Real, n::Int)
+    
+    clear_bg(cr,rect_width,rect_height);
+    px,py = randpos(n,rect_width,rect_height);
+
+    cc = radius + 3.0;
+
+    s1 = Cairo.CairoARGBSurface(cc*2,cc*2);
+    c1 = Cairo.CairoContext(s1);
+    rectangle(c1,0,0,2*cc,2*cc);
+    set_source_rgba(c1,0,0,0,0);
+    paint(c1);
+    new_path(c1);
+    arc(c1,radius+1,radius+1,radius,0,2*pi);
+    set_source_rgb(c1,0,0,1.0);
+    fill_preserve(c1);
+    set_source_rgb(c1,0,0,0);
+    set_line_width(c1,1.0);
+    stroke(c1);
+
+    p = Cairo.CairoPattern(s1);
+        
+    for i=1:n
+        save(cr);
+        translate(cr,px[i]-cc,py[i]-cc);
+        set_source(cr,p);
+        paint(cr);
+        restore(cr);
+    end
+end
+
 
 # lines0, random x,y lines
 function lines0(cr::CairoContext, rect_width::Real, rect_height::Real, width::Real, n::Int)
@@ -152,12 +308,51 @@ function lines2(cr::CairoContext, rect_width::Real, rect_height::Real, width::Re
 
     set_source_rgb(cr, 0, 0, 1.0);
     set_line_width(cr,width)
-    
-    move_to(cr,px[1],py[1]);
+
+    new_path(cr);    
     for i=1:n-1
-        new_path(cr);
         move_to(cr,px[i],py[i]);
         line_to(cr,px[i+1],py[i+1]);
+        stroke(cr);
+    end      
+end
+
+# lines3, x sorted, in clusters of 100
+function lines3(cr::CairoContext, rect_width::Real, rect_height::Real, width::Real, n::Int)
+    
+    clear_bg(cr,rect_width,rect_height);
+    px,py = randpos(n,rect_width,rect_height);
+    
+    px = sort(px);
+
+    set_source_rgb(cr, 0, 0, 1.0);
+    set_line_width(cr,width);
+    new_path(cr);
+    for c = 1:100:n
+        move_to(cr,px[c],py[c]);
+            for i=c:min(c+100,n)
+            line_to(cr,px[i],py[i]);
+        end
+        stroke(cr);
+    end      
+end
+
+# lines4, x sorted, in clusters of 1000
+function lines4(cr::CairoContext, rect_width::Real, rect_height::Real, width::Real, n::Int)
+    
+    clear_bg(cr,rect_width,rect_height);
+    px,py = randpos(n,rect_width,rect_height);
+    
+    px = sort(px);
+
+    set_source_rgb(cr, 0, 0, 1.0);
+    set_line_width(cr,width);
+    new_path(cr);
+    for c = 1:1000:n
+        move_to(cr,px[c],py[c]);
+            for i=c:min(c+1000,n)
+            line_to(cr,px[i],py[i]);
+        end
         stroke(cr);
     end      
 end
--- test/test_speed.jl
+++ test/test_speed.jl
@@ -1,29 +1,69 @@
 using Cairo
 import shape_functions
 
-print("cairo version:",ccall((:cairo_version,Cairo._jl_libcairo),Int32,()),"\n")
+#function test_all()
 
-#size_surface = [256,512,1024] #three sizes of a surface
-size_surface = [512]
+	print("cairo version: ",ccall((:cairo_version,Cairo._jl_libcairo),Int32,()),"\n");
 
-for s_size in size_surface
+	size_surface = [512];#,512,1024]; #three sizes of a surface
 
-	s = Cairo.CairoARGBSurface(s_size,s_size);
-	c = Cairo.CairoContext(s)
+	paint_width = [0.5,1.0,3.0,5.0];
 
-	print("Surface Size: ",s_size,"\n");
+	shapes = [ddots1, ddots2, ddots3, ddots4, ddots5, 
+			rdots1, rdots2, rdots3, rdots4, rdots5,
+			lines0, lines1, lines2, lines3, lines4];
 
-	for w in [0.5,1.0,3.0]
+	n_elements = [100,300,1000,3000,10000,30000,100000];
 
-		print("Paint Width: ",w,"\n");		
+	n_tests = length(size_surface) * length(paint_width) * length(shapes);
+	test_data = zeros(n_tests+1,3 + length(n_elements));
 
-		for m in [ddots1, ddots2, ddots3, ddots4, lines0, lines1, lines2]
+	test_data[1,3+(1:length(n_elements))] = n_elements;
 
-			for n in [100,300,1000,3000,10000,30000,100000]
-				print(m," ",@sprintf("%6d",n),"  ");
-        		@time m(c,s_size,s_size,w,n)
-        	end
-        	print("\n");
-    	end
+	save_flag = false;
+
+	i = 1;
+	f = 1;
+
+	for s_size in size_surface
+
+		s = Cairo.CairoARGBSurface(s_size,s_size);
+		c = Cairo.CairoContext(s);
+
+		print("Surface Size: ",s_size,"\n");
+
+		for w in paint_width
+
+			print("Paint Width: ",w,"\n");		
+
+			for m_index in 1:length(shapes)
+
+				test_data[i,1] = s_size;
+				test_data[i,2] = w;
+				test_data[i,3] = m_index;
+				m = shapes[m_index];
+
+				for n_index in 1:length(n_elements)
+
+					n = n_elements[n_index];
+					t = @elapsed(m(c,s_size,s_size,w,n));
+					print(m," ",@sprintf("%6d",n),"  elapsed time: ",@sprintf("%f seconds",t),"\n");
+      				test_data[i+1,3+n_index] = t;
+
+	        		if save_flag 
+    	    			write_to_png(s,@sprintf("test_speed_%06d.png",f));
+        			end
+        			f += 1;
+
+        		end
+        		print("\n");
+        		i += 1;
+
+    		end
+		end
 	end
-end
+
+	#test_data;
+#end;
+
+#print(test_all);
