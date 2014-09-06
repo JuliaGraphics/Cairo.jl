c3aa6eaffb07399ca741229e07700ae234dc5d73
Tony Kelman <tony@kelman.net>
2014-09-06 07:43:04 -0700

    fix winrpm package name for zlib
    see https://build.opensuse.org/package/rdiff/windows:mingw:win64/mingw64-zlib?linkrev=base&rev=19

--- deps/build.jl
+++ deps/build.jl
@@ -23,7 +23,7 @@ deps = [
     using WinRPM
     provides(WinRPM.RPM,"libpango-1_0-0",[pango,pangocairo],os = :Windows)
     provides(WinRPM.RPM,["glib2", "libgobject-2_0-0"],gobject,os = :Windows)
-    provides(WinRPM.RPM,"zlib",zlib,os = :Windows)
+    provides(WinRPM.RPM,"zlib1",zlib,os = :Windows)
     provides(WinRPM.RPM,["libcairo2","libharfbuzz0"],cairo,os = :Windows)
 end
 
